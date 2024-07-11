61a1fe923629dc2300fedbc49194225031c99428
MrSinkaaa <sinkapersinka@gmail.com>
2024-07-11 14:40:51 +0300

    feat: add few methods to interface

--- src/main/java/ru/mrsinkaaa/cloudfilestorage/service/interfaces/IFileService.java
+++ src/main/java/ru/mrsinkaaa/cloudfilestorage/service/interfaces/IFileService.java
@@ -4,6 +4,7 @@ package ru.mrsinkaaa.cloudfilestorage.service.interfaces;
 import org.springframework.web.multipart.MultipartFile;
 import ru.mrsinkaaa.cloudfilestorage.dto.FileDTO;
 import ru.mrsinkaaa.cloudfilestorage.entity.File;
+import ru.mrsinkaaa.cloudfilestorage.entity.Folder;
 import ru.mrsinkaaa.cloudfilestorage.entity.User;
 
 
@@ -12,6 +13,8 @@ import java.util.List;
 public interface IFileService {
 
     FileDTO findByFileName(String fileName);
+    File findFileByOwnerIdAndId(Long ownerId, Long id);
+    List<FileDTO> findByFolderId(Folder folderId);
     File renameFile(String oldFileName, String newFileName);
     File deleteFile(User user, Long id);
 }
